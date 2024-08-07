b6a4045185184064f7528749868739bd11ca4eaf
arielRosero <ariel.rosero@epn.edu.ec>
2024-08-07 08:55:19 -0500

    ListTaskLogsAndKPIs
    ArielR

--- proyect/app/companies/[idtenant]/[idEmployee]/[IdTask]/page.jsx
+++ proyect/app/companies/[idtenant]/[idEmployee]/[IdTask]/page.jsx
@@ -16,7 +16,7 @@ export default function PageTaskLogsKpis() {
     const getTasksLogsList = async () => {
         try {
             //Obtener tareas de empleados con x-tenant-id
-            const response = await fetch(`http://localhost:3000/employees/${employeeId}/tasks`, {
+            const response = await fetch(`http://localhost:3000/employees/${employeeId}/task/${taskId}/tasklogs`, {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
@@ -29,8 +29,31 @@ export default function PageTaskLogsKpis() {
             }
 
             const data = await response.json();
-            setTasks(data);
+            setTaskLogs(data);
             console.log("Respuesta: ", data);
+        } catch (error) {
+            console.error("Fetch error: ", error);
+        }
+    };
+
+    const getKPIsList = async () => {
+        try {
+            //Obtener tareas de empleados con x-tenant-id
+            const response = await fetch(`http://localhost:3000/employees/${employeeId}/task/${taskId}/tasklogs`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-tenant-id": tenantId, //Pasar el id de la empresa como x-tenant-id
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data1 = await response.json();
+            setKpis(data1);
+            console.log("Respuesta: ", data1);
         } catch (error) {
             console.error("Fetch error: ", error);
         }
@@ -41,11 +64,22 @@ export default function PageTaskLogsKpis() {
             return [...keys, ...Object.keys(task)];
         }, []);
 
-        return [...new Set(allKeys)];
+        return [...new Set(allKeys)].filter((key) => key !== '_id');
     };
 
     const tableTaskLogsHeaders = getTableTaskLogsHeaders();
     const hasTasksLogs = tasklogs.length > 0;
+
+    const getTableKPIsHeaders = () => {
+        const allKeys = kpis.reduce((keys, task) => {
+            return [...keys, ...Object.keys(task)];
+        }, []);
+
+        return [...new Set(allKeys)].filter((key) => key !== '_id');
+    };
+
+    const tableKPIsHeaders = getTableKPIsHeaders();
+    const hasKPIs = kpis.length > 0;
 
     const handleButtonClickKPI = () => {
         router.push(`/companies/${params.idtenant}/${params.idEmployee}/${params.IdTask}/createKPI`);
@@ -73,44 +107,103 @@ export default function PageTaskLogsKpis() {
                 ⬅️ Back
             </Link>
 
-            <div className='mt-10'>
-                <h2 className='text-center text-[32px] text-rose-900'>List TaskLogs</h2>
-                <p className='text-[24px] text-rose-950'>Employee: {employeeId}</p>
-                <p className='text-[24px] text-rose-950'>Task: {taskId}</p>
+            <div className='bg-blue-400 w-full flex mb-3'>
+
+                <div>
+                    <div className='mt-10 flex flex-col items-center justify-center'>
+                        <h2 className='text-[28px] text-rose-900'>List TaskLogs and KPI's</h2>
+                        <p className='text-[20px] text-rose-950'>Employee: {employeeId}</p>
+                        <p className='text-[20px] text-rose-950'>Task: {taskId}</p>
+                    </div>
+
+                    <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md bg-opacity-70">
+                        <div className="mb-4">
+                            <label htmlFor="tenantName" className="block text-sm font-medium text-gray-700 mb-2">
+                                Tenant Name:
+                            </label>
+                            <input
+                                id="tenantName"
+                                type="text"
+                                placeholder="CompanyExample"
+                                className="text-black block w-full border border-gray-300 rounded-lg px-3 py-2 placeholder-gray-400 focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 transition"
+                                value={tenantId}
+                                onChange={e => {
+                                    // Limitar la longitud del valor a 400 caracteres
+                                    if (e.target.value.length <= 90) {
+                                        setTenantId(e.target.value);
+                                    }
+                                }}
+                            />
+                        </div>
+                        <button
+                            onClick={() => {getTasksLogsList(); getKPIsList()}}
+                            className="w-full py-2 bg-green-500 text-white rounded-lg font-semibold hover:bg-green-600 transition-colors"
+                        >
+                            Listar TasksLogs and KPIs
+                        </button>
+                    </div> <br />
+
+                    <div className='flex justify-evenly'>
+                        <Link href={`/companies/${params.idtenant}/${params.idEmployee}/CreateTask`}
+                            className="py-2 bg-green-500 text-white rounded-lg font-semibold hover:bg-green-600 transition-colors w-[150px] text-center"
+                        >
+                            Registrar Log
+                        </Link>
+
+                        <Link href={`/companies/${params.idtenant}/${params.idEmployee}/CreateTask`}
+                            className="py-2 bg-green-500 text-white rounded-lg font-semibold hover:bg-green-600 transition-colors w-[150px] text-center"
+                        >
+                            Registrar KPI
+                        </Link>
+                    </div>
+
+                </div>
+
+                <div>
+                    <div className="flex justify-center">
+                        <div className="overflow-x-auto">
+                            <table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
+                                <thead className="bg-gradient-to-r from-indigo-600 to-purple-600 text-white">
+                                    <tr>
+                                        {tableKPIsHeaders.map((header) => (
+                                            <th key={header} className="py-3 px-6 text-left uppercase tracking-wider">
+                                                {header.replace(/_/g, ' ')}
+                                            </th>
+                                        ))}
+                                        {hasKPIs && (
+                                            <th className="py-3 px-3 text-left uppercase tracking-wider">Actions</th>
+                                        )}
+                                    </tr>
+                                </thead>
+                                <tbody className='text-black'>
+                                    {kpis.map((kpi, index) => (
+                                        <tr
+                                            key={kpi._id}
+                                            className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}
+                                        >
+                                            {tableKPIsHeaders.map((header) => (
+                                                <td key={header} className="py-3 px-6">
+                                                    {renderCellContent(header, kpi[header])}
+                                                </td>
+                                            ))}
+                                            <td className="py-3 px-2">
+                                                <button
+                                                    onClick={() => handleButtonClickKPI()}
+                                                    className="py-1 px-3 bg-blue-500 text-white rounded-full hover:bg-blue-600 mb-2"
+                                                >
+                                                    Edit
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
-            <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md bg-opacity-70">
-                <div className="mb-4">
-                    <label htmlFor="tenantName" className="block text-sm font-medium text-gray-700 mb-2">
-                        Tenant Name:
-                    </label>
-                    <input
-                        id="tenantName"
-                        type="text"
-                        placeholder="CompanyExample"
-                        className="text-black block w-full border border-gray-300 rounded-lg px-3 py-2 placeholder-gray-400 focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 transition"
-                        value={tenantId}
-                        onChange={e => {
-                            // Limitar la longitud del valor a 400 caracteres
-                            if (e.target.value.length <= 90) {
-                                setTenantId(e.target.value);
-                            }
-                        }}
-                    />
-                </div>
-                <button
-                    onClick={getTasksLogsList}
-                    className="w-full py-2 bg-green-500 text-white rounded-lg font-semibold hover:bg-green-600 transition-colors"
-                >
-                    Listar TasksLogs
-                </button>
-            </div> <br />
-
-            <Link href={`/companies/${params.idtenant}/${params.idEmployee}/CreateTask`}
-                className="py-2 bg-green-500 text-white rounded-lg font-semibold hover:bg-green-600 transition-colors w-[200px] text-center"
-            >
-                Asignar Tarea Empleado
-            </Link><br />
             {/*---------- tabla dinámica para tasks con campos variables ----------*/}
             <div className="flex justify-center">
                 <div className="overflow-x-auto">
@@ -123,7 +216,7 @@ export default function PageTaskLogsKpis() {
                                     </th>
                                 ))}
                                 {hasTasksLogs && (
-                                    <th className="py-3 px-6 text-left uppercase tracking-wider">Actions</th>
+                                    <th className="py-3 px-3 text-left uppercase tracking-wider">Actions</th>
                                 )}
                             </tr>
                         </thead>
@@ -133,7 +226,7 @@ export default function PageTaskLogsKpis() {
                                     key={taskLog._id}
                                     className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}
                                 >
-                                    {tableHeaders.map((header) => (
+                                    {tableTaskLogsHeaders.map((header) => (
                                         <td key={header} className="py-3 px-6">
                                             {renderCellContent(header, taskLog[header])}
                                         </td>
@@ -143,13 +236,7 @@ export default function PageTaskLogsKpis() {
                                             onClick={() => handleButtonClickKPI()}
                                             className="py-1 px-3 bg-blue-500 text-white rounded-full hover:bg-blue-600 mb-2"
                                         >
-                                            KPI
-                                        </button>
-                                        <button
-                                            onClick={() => handleButtonClickLogs()}
-                                            className="py-1 px-3 bg-orange-500 text-white rounded-full hover:bg-orange-600"
-                                        >
-                                            Logs
+                                            Edit
                                         </button>
                                     </td>
                                 </tr>
