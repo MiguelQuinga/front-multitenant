1af2e531e5cc27647fa4eda9680f8863ba34356e
MiguelMuenala <mickaelo.1755@gmail.com>
2024-08-08 12:28:26 -0500

    api para obtener kpi
    api para obtener kpi

--- proyect/app/companies/[idtenant]/[idEmployee]/[IdTask]/[id_kpi]/kpi-evaluation/page.jsx
+++ proyect/app/companies/[idtenant]/[idEmployee]/[IdTask]/[id_kpi]/kpi-evaluation/page.jsx
@@ -17,6 +17,7 @@ export default function KpiEvaluation() {
     const [selectedField, setSelectedField] = useState("");
     const [startDate, setStartDate] = useState('')
     const [endDate, setEndDate] = useState('')
+    const [kpiInformation, setKpiInformation] = useState('')
 
     useEffect(() => {
 
@@ -69,6 +70,30 @@ export default function KpiEvaluation() {
             console.error("Fetch error: ", error);
         }
     };
+    const getKPIbyID = async () => {
+        console.log("entro a getKPIEcaluation")
+        try {
+            //Obtener tareas de empleados con x-tenant-id
+            const response = await fetch(`http://localhost:3000/employees/${params.idEmployee}/tasks/${params.IdTask}/kpis/${params.id_kpi}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-tenant-id": params.idtenant, //Pasar el id de la empresa como x-tenant-id
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data1 = await response.json();
+            console.log("KpiInformation: ", data1)
+            setKpiInformation(data1);
+
+        } catch (error) {
+            console.error("Fetch error: ", error);
+        }
+    };
     const handleSelectChange = (event) => {
         setSelectedField(event.target.value);
     };
@@ -112,19 +137,24 @@ export default function KpiEvaluation() {
                             </option>
                         ))}
                     </select>
-                <button className='text-black bg-green-300 hover:bg-green-500 font-semibold py-2 px-4 rounded-full shadow-md transition-all' onClick={getFields}>
-                    Obtener campos
-                </button><br />
+                    <button className='text-black bg-green-300 hover:bg-green-500 font-semibold py-2 px-4 rounded-full shadow-md transition-all' onClick={getFields}>
+                        Obtener campos
+                    </button><br />
                 </div><br />
 
-                <button className='text-black bg-green-300 hover:bg-green-500 font-semibold py-2 px-4 rounded-full shadow-md transition-all' onClick={getKPIEcaluation}>
+                <button className='text-black bg-green-300 hover:bg-green-500 font-semibold py-2 px-4 rounded-full shadow-md transition-all'
+                    onClick={() => {
+                        getKPIEcaluation()
+                        getKPIbyID()
+                    }}>
                     Calcular porcentaje
                 </button><br />
             </div><br />
 
             <div className='h-[300px] w-[500px] bg-white bg-opacity-50 p-10 rounded-lg shadow-lg backdrop-blur-sm'>
-                <p className='text-center text-lg font-bold mb-4'>Objetivo: {kpiPercentage}%</p>
-                <p className='text-center text-lg font-bold mb-4'>Cantidad de días: {kpiPercentage}%</p>
+            <p className='text-center text-lg font-bold mb-4'>Descripción: {kpiInformation.description}</p>
+                <p className='text-center text-lg font-bold mb-4'>Objetivo: {kpiInformation.target}</p>
+                <p className='text-center text-lg font-bold mb-4'>Cantidad de días: {kpiInformation.timeUnit}</p>
                 <p className='text-center text-lg font-bold mb-4'>Barra de progreso: {kpiPercentage}%</p>
                 <div className="relative pt-1">
                     <div className="overflow-hidden h-4 mb-4 text-xs flex rounded bg-gray-200">
